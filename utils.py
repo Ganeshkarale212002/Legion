dc4b4a815370d5327b94bcf6abc21c4daa5773d1
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-08-17 13:18:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -375,3 +375,38 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://{SHORTNER}/api'
+    params = {
+            "api": SHORTNER_API,
+            "url": link,
+        }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{SHORTNER}/api?api={SHORTNER_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{SHORTNER}/api?api={SHORTNER_API}&link={link}'
+
+
+
+
+
+
+
+
+
+
