508ee8e6642a1414131f3716385b43aff902459c
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-09-10 19:36:39 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -54,7 +54,7 @@ async def save_group(bot, message):
                         pass
                 temp.MELCOW['welcome'] = await message.reply_photo(
                 photo="https://graph.org/file/75d4fcc66598ea1ef4941.jpg",                                              
-                                                 caption=f'<b><i>🔆 "HEROFLiX • GROUP" 亗 🔆\n•───────────────────• \n⚜Hey {u.mention}, Welcome To HeroFlix • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>',
+                                                 caption=f'<b><i>🔆 "HEROFLiX • GROUP" 亗 🔆\n•───────────────────• \n⚜Hey {u.mention}, Welcome To HeroFlix • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here...</i></b>',
                                                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❓How To Download❓', url=f'https://telegram.me/{DWLD}') ] ] )
                 )
                 await asyncio.sleep(900)
