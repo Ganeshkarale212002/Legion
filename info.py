c16d54423baaadb9ddd495582dcb066edad63803
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-09-22 19:25:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '5997447')
 API_HASH = environ.get('API_HASH', '04fbdff99ed5edff6dfdcc4ef710d04e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6682653076:AAGdzLZs_hiee3CFDYaGKH1CuvLLp1FenBI')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6682653076:AAGrB5NO3LbYEQoWG0PlgCWPUa-henTjH34')
 
 DWLD = environ.get("DWLD", 'FHDmovies24x7Official/3536')
 
